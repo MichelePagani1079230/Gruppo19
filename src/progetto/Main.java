451eaa25289c025d20899076de4ebd3cf8853a67
ZanottiMatteo <m.zanotti13@studenti.unibg.it>
2023-03-30 17:46:24 +0200

    Prova altro pc

--- src/progetto/Main.java
+++ src/progetto/Main.java
@@ -1,10 +1,14 @@
 package progetto;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Prova");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Scrivi:");
+		String line=sc.nextLine();
+		System.out.println(line);
 		System.out.println("Funziona");
 		System.out.println("https://github.com/MichelePagani1079230/Gruppo19.git");
 		int a= 1;
