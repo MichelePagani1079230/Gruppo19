24fe6a3d4712d3d9776dea5dbfc27b9e266a1bd2
ZanottiMatteo <m.zanotti13@studenti.unibg.it>
2023-03-30 13:09:33 +0200

    provaa

--- src/progetto/Main.java
+++ src/progetto/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Prova");
 		System.out.println("Funziona");
-		
+		System.out.println("https://github.com/MichelePagani1079230/Gruppo19.git");
 		int a= 1;
 		
 	}
